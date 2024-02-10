78f0da9628465f9eaec3ae14f4bd76904549e2d3
Tim Hiebenthal <tim.hiebenthal@project-a.com>
2024-02-10 10:39:55 +0100

    feat: prep demo

--- data_imports/dummy.py
+++ data_imports/dummy.py
@@ -5,9 +5,8 @@ pipeline = dlt.pipeline(
     pipeline_name="dwh",
     destination=dlt.destinations.duckdb(credentials="database/dwh.duckdb"),
     dataset_name="raw_dummy",
-    import_schema_path="data_imports/schemas/import/",
-    export_schema_path="data_imports/schemas/export",
-    full_refresh=True,
+    # import_schema_path="data_imports/schemas/import/",
+    # export_schema_path="data_imports/schemas/export",
 )
 
 
@@ -30,27 +29,25 @@ data = [
         "position": "G",
         "height_feet": "6.6",
     },
-    {
-        "id": 3,
-        "name": "Stephen Curry",
-        "position": "G",
-        "height_feet": "5",
-        # "accomplishments": [
-        #     {"year": 2015, "award": "MVP"},
-        #     {"year": 2015, "award": "Champion"},
-        #     {"year": 2016, "award": "MVP"},
-        #     {"year": 2017, "award": "MVP"},
-        # ],
-    },
-    {
-        "id": 5,
-        "name": "Kawhi Leonard",
-        "position": "F",
-        "height_feet": "6.7",
-    },
+    # {"id": 5, "name": "Kawhi Leonard", "position": "F", "height_feet": "6.7"},
+    # {
+    #     "id": 3,
+    #     "name": "Stephen Curry",
+    #     "position": "G",
+    #     "height_feet": "5",
+    #     "accomplishments": [
+    #         {"year": 2015, "award": "MVP"},
+    #         {"year": 2015, "award": "Champion"},
+    #         {"year": 2016, "award": "MVP"},
+    #         {"year": 2017, "award": "MVP"},
+    #     ],
+    # },
 ]
 
 load_info = pipeline.run(
-    data, table_name="players", write_disposition="merge", primary_key="id"
+    data,
+    table_name="players",
+    # write_disposition="merge",
+    # primary_key="id",
 )
 print("done")
